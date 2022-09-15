13805bc77e560d1171d612faa2b7b18be307918a
unknown <hieu2233ckh@gmail.com>
2022-09-15 16:30:43 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
+      <LinearGradient colors={["white", "#50C787"]} style={styles.linearGradient}>
+        <View style={{flex: 1}}></View>
+        <View style={{flex: 3}}>
+          <Image source={require('./img/circle.png')}/>
+        </View>
+        <View style={{flex: 1, }}>
+          <Text style={{textAlign: "center", fontWeight: "bold", fontSize: 24}}>
+            GROW
+          </Text>
+          <Text style={{textAlign: "center", fontWeight: "bold", fontSize: 24}}>
+            YOUR BUSINESS
+          </Text>
+        </View>
+        <View style={{flex: 1}}>
+          <Text style={{textAlign: "center", fontWeight: "bold"}}>
+            We will help you to grow your business using
+          </Text>
+          <Text style={{textAlign: "center", fontWeight: "bold"}}>
+            online server
+          </Text>
+        </View>
+        <View style={{flexDirection: "row", justifyContent: "space-between", width: "50%"}}>
+          <Button title='LOGIN' color = "#C7B350"></Button>
+          <Button title='SIGN UP' color = "#C7B350"></Button>
+        </View>
+        <View style={{flex: 1}}>
+        <View style={{flex: 1}}></View>
+          <Text style={{textAlign: "center", fontWeight: "bold",  fontSize: 20}}>
+            HOW WE WORK?
+          </Text>
+        </View>
+        <View style={{flex: 1}}></View>
+      </LinearGradient>
     </View>
   );
 }
@@ -13,8 +46,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: ["purple","white"],
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  linearGradient: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 220,
+    width: 410
+  }
+})
